aa8c31da2a234ba5f877b8cd5a350b42b74a0bc6
Siddharth Dronachary <siddudrona@gmail.com>
2026-07-09 19:20:42 +0530

    Add Transform to sum tree program for binary trees

---
+++ Trees/BinaryTree/transformToSumTree.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* BuildTree(vector<int> preorder) {
+    idx++;
+
+    if(preorder[idx] == -1) {
+        return NULL;
+    }
+
+    Node* root = new Node(preorder[idx]);
+    root->left = BuildTree(preorder);
+    root->right = BuildTree(preorder);
+ 
+    return root; 
+}
+
+int sumTree(Node* root) {
+  if(root == NULL) return 0;
+
+  int leftSum = sumTree(root->left);
+  int rightSum = sumTree(root->right);
+
+  root->data += leftSum + rightSum;
+
+  return root->data;
+}
+
+int main() {
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+    Node* root = BuildTree(preorder);
+
+    cout << sumTree(root) << endl;
+
+    return 0;
+}
